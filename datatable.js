1b133bfe0098a91364f813400936e7cdf76d281f
Avi Parshan <avipars@users.noreply.github.com>
2022-04-10 23:00:01 +0300

    d

--- datatable.js
+++ datatable.js
@@ -6,14 +6,6 @@ $(document).ready(function() {
             search: "חיפוש - Search:"
         }
     });
-    // $("table[id*='Table']").DataTable({
-    //     "paging": false,
-    //     select: true,
-    //     responsive: true,
-    //     language: {
-    //         search: "חיפוש - Search:"
-    //     }
-    // });
 
     console.log("Contribute to our project https://github.com/avipars/OsherAd");
     console.log("we are on telegram too " + "https://t.me/osheradnews");

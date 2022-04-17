889fb55dcd9bdae34f6d3eac61d8cfc8dda0934c
Avi Parshan <avipars@users.noreply.github.com>
2022-04-17 22:49:14 +0300

    wip

--- datatable.js
+++ datatable.js
@@ -1,12 +1,42 @@
 $(document).ready(function() {
-    $('#locationTable').DataTable({
-        "paging": false,
-        select: true,
-        language: {
-            search: "חיפוש - Search:"
-        }
-    });
+    // $('#locationTable').DataTable({
+    //     "paging": false,
+    //     select: true,
+    //     language: {
+    //         search: "חיפוש - Search:"
+    //     }
+    // });
 
     console.log("Contribute to our project https://github.com/avipars/OsherAd");
     console.log("we are on telegram too " + "https://t.me/osheradnews");
+    var dayOfWeek = new Date().getDay();
+    var num = -1; //select which P to show
+
+    if (dayOfWeek >= 0 && dayOfWeek <= 3) { //sunday through tuesday
+        num = 0;
+    } else if (dayOfWeek > 3 && dayOfWeek <= 4) { //wednesday through thursday
+        num = 1;
+        //w,t 
+    } else if (dayOfWeek == 5) { //friday  - check months for summer/winter
+        var month = new Date().getMonth() + 1; //1-12 not 0-11
+        if (month >= 3 && month <= 10) { //summer from march to 
+            num = 3;
+        } else { //winter
+            num = 2;
+        }
+    } else {
+        num = -1; //don't show
+    }
+
+    //data table add attribute to the line with the right day of week 
+    $('.hours p').each(function() {
+        var row = $(this); //get current row
+        if (row.index() == num && num != -1) {
+            row.addClass('today'); //highlighter 
+        }
+    });
+
+    $('#locationTable tr').click(function() {
+        $('.hidden').toggle(); //toggle the view
+    })
 });
